5d04874f6b56b28e3f7aa973d0b12ea151a4640a
Alexander Plavin <alexander@plav.in>
2023-01-27 02:09:07 +0300

    add @inferred

--- test/runtests.jl
+++ test/runtests.jl
@@ -478,24 +478,24 @@ if isdefined(Base, :get_extension)  # some 1.9 version
         @test sa2 === @SVector [3.0, 5.0, 7.0, 9.0]
 
         ma = @MMatrix [2.0 4.0; 6.0 8.0]
-        ma2 = ConstructionBase.constructorof(typeof(ma))((1, 2, 3, 4))
+        ma2 = @inferred ConstructionBase.constructorof(typeof(ma))((1, 2, 3, 4))
         @test ma2 isa MArray{Tuple{2,2},Int,2,4}
         @test all(ma2 .=== @MMatrix [1 3; 2 4])
 
         sz = SizedArray{Tuple{2,2}}([1 2;3 4])
-        sz2 = ConstructionBase.constructorof(typeof(sz))([:a :b; :c :d]) 
+        sz2 = @inferred ConstructionBase.constructorof(typeof(sz))([:a :b; :c :d]) 
         @test sz2 == SizedArray{Tuple{2,2}}([:a :b; :c :d])
         @test typeof(sz2) <: SizedArray{Tuple{2,2},Symbol,2,2}
 
         for T in (SVector, MVector)
-            @test ConstructionBase.constructorof(T)((1, 2, 3))::T == T((1, 2, 3))
-            @test ConstructionBase.constructorof(T{3})((1, 2, 3))::T == T((1, 2, 3))
-            @test ConstructionBase.constructorof(T{3})((1, 2))::T == T((1, 2))
-            @test ConstructionBase.constructorof(T{3, Symbol})((1, 2, 3))::T == T((1, 2, 3))
-            @test ConstructionBase.constructorof(T{3, Symbol})((1, 2))::T == T((1, 2))
-            @test ConstructionBase.constructorof(T{3, X} where {X})((1, 2, 3))::T == T((1, 2, 3))
-            @test ConstructionBase.constructorof(T{3, X} where {X})((1, 2))::T == T((1, 2))
-            @test ConstructionBase.constructorof(T{X, Symbol} where {X})((1, 2, 3))::T == T((1, 2, 3))
+            @test @inferred(ConstructionBase.constructorof(T)((1, 2, 3)))::T == T((1, 2, 3))
+            @test @inferred(ConstructionBase.constructorof(T{3})((1, 2, 3)))::T == T((1, 2, 3))
+            @test @inferred(ConstructionBase.constructorof(T{3})((1, 2)))::T == T((1, 2))
+            @test @inferred(ConstructionBase.constructorof(T{3, Symbol})((1, 2, 3)))::T == T((1, 2, 3))
+            @test @inferred(ConstructionBase.constructorof(T{3, Symbol})((1, 2)))::T == T((1, 2))
+            @test @inferred(ConstructionBase.constructorof(T{3, X} where {X})((1, 2, 3)))::T == T((1, 2, 3))
+            @test @inferred(ConstructionBase.constructorof(T{3, X} where {X})((1, 2)))::T == T((1, 2))
+            @test @inferred(ConstructionBase.constructorof(T{X, Symbol} where {X})((1, 2, 3)))::T == T((1, 2, 3))
         end
 
         sv = SVector(1, 2)
