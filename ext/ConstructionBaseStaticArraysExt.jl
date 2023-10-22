52b813dd45dfbf0db6fa27145921885755407696
MilesCranmer <miles.cranmer@gmail.com>
2023-10-22 22:15:14 +0100

    Extension cleanup

--- ext/ConstructionBaseStaticArraysExt.jl
+++ ext/ConstructionBaseStaticArraysExt.jl
@@ -9,9 +9,9 @@ else
 end
 
 # general static arrays need to keep the size parameter
-ConstructionBase.constructorof(sa::Type{<:SArray{S}}) where {S} = SArray{S}
-ConstructionBase.constructorof(sa::Type{<:MArray{S}}) where {S} = MArray{S}
-ConstructionBase.constructorof(sa::Type{<:SizedArray{S}}) where {S} = SizedArray{S}
+ConstructionBase.constructorof(::Type{<:SArray{S}}) where {S} = SArray{S}
+ConstructionBase.constructorof(::Type{<:MArray{S}}) where {S} = MArray{S}
+ConstructionBase.constructorof(::Type{<:SizedArray{S}}) where {S} = SizedArray{S}
 
 # static vectors don't even need the explicit size specification
 ConstructionBase.constructorof(::Type{<:SVector}) = SVector
